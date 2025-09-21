d9bd6539407d100f0e72131b04f0596f3a5a7f41
mirkobechini <mirkobechini@gmail.com>
2025-09-21 11:43:01 +0200

    Add error handling middleware for server and not found errors

--- server.js
+++ server.js
@@ -4,15 +4,28 @@ const PORT = 3000
 
 //Routers
 const postsRouter = require('./routers/posts')
+
+//Middlewares
+const serverError = require('./middleware/serverError')
+const notFoundError = require('./middleware/notFoundError')
+
 app.use(express.json())
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 })
 
+//Use routers
 app.use('/api/posts', postsRouter)
 
 
+
 app.get("/", (req, res) => {
-    res.send("home")
+    res.send("Welcome in my Blog server")
 })
+
+
+//Error Middlewares
+app.use(serverError)
+
+app.use(notFoundError)
